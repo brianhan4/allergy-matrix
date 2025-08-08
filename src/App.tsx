e01f119654485903851a3c75a9c0922dac899e7c
Brian Han <brianhan4@gmail.com>
2025-08-08 12:21:12 -0700

    cleaned up unncessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
-import { useEffect } from 'react';
-import { db } from './firebase';
-import { collection, getDocs } from 'firebase/firestore';
+import React from 'react';
 
 function App() {
-  useEffect(() => {
-    const checkFirebaseConnection = async () => {
-      try {
-        const querySnapshot = await getDocs(collection(db, "menu-items"));
-        console.log("Successfully connected to Firestore!");
-        querySnapshot.forEach((doc) => {
-          console.log("Found a document with ID: ", doc.id);
-        });
-      } catch (error) {
-        console.error("Error connecting to Firebase:", error);
-      }
-    };
-
-    checkFirebaseConnection();
-  }, []);
-
   return (
-    <div>
-      <h1>My Allergy Matrix App</h1>
-    </div>
+    <>
+      <h1>Allergy Matrix App</h1>
+    </>
   );
 }
 
